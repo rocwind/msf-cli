d6ecd49796fa7f0aa3efae08e78d7e07abcb3cbe
Roc Wu <rocwind@gmail.com>
2021-04-03 14:43:44 +0800

    feat: support include/exclude sync files and sync modes

--- README.md
+++ README.md
@@ -11,5 +11,14 @@ msf(My Sync Folders) listens to changes between 2 folders and try to keep them i
 `msf ~/my-package ~/my-project/node_modules/my-package`
 use `msf --help` to see all supported options
 
+### .msfignore
+a `.msfignore` file under source folder, follows `.gitignore` syntax can help to ignore files from being synced.
+since msf is based on [watchman](https://facebook.github.io/watchman/), a `.watchmanconfig` with below example contents can also help to ignore files:
+```
+{
+  "ignore_dirs": ["node_modules"]
+}
+```
+
 ## Why
 Symlinks are not supported in react-native currently, [check this thread](https://github.com/facebook/watchman/issues/105). Copy files is almost the only option to develop a shared package for RN project, the reason we want it bi-directional is sometimes, editing native code in RN project (xcode/Android studio) is more convenient to try things out and we want the edits can be synced back to source package.
--- src/cli.ts
+++ src/cli.ts
@@ -2,34 +2,71 @@
 import { join } from 'path';
 import yargs from 'yargs';
 import { cyan, green, red } from 'chalk';
-import { existsSync } from 'fs-extra';
+import { existsSync, readFileSync, removeSync, ensureDirSync } from 'fs-extra';
+import ignore, { Ignore } from 'ignore';
 import { startWatch, Handler } from './client';
 import { syncTo, rmFile } from './sync';
 import { getLogger, Logger } from './logger';
 
-export const argv = yargs.usage('usage: msf source_folder target_folder').check((argv) => {
-    if (argv._.length !== 2) {
-        throw new Error(
-            red('Error: invalid command arguments, msf can only sync between 2 folders'),
-        );
-    }
-    argv._.forEach((path: string) => {
-        const exists = existsSync(path);
-        if (!exists) {
-            throw new Error(red(`Error: ${path} is not exists`));
+export const argv = yargs
+    .usage('usage: msf source_folder target_folder')
+    .check((argv) => {
+        if (argv._.length !== 2) {
+            throw new Error(
+                red('Error: invalid command arguments, msf can only sync between 2 folders'),
+            );
         }
-    });
+        argv._.forEach((path: string) => {
+            const exists = existsSync(path);
+            if (!exists) {
+                throw new Error(red(`Error: ${path} is not exists`));
+            }
+        });
 
-    return true;
-}).argv;
+        return true;
+    })
+    .option('files', {
+        alias: '-f',
+        type: 'array',
+        describe: 'syncs only files that matches given glob pattern',
+    })
+    .option('mode', {
+        alias: '-m',
+        choices: ['update-both', 'update', 'mirror'],
+        describe: 'sync mode: bi-directional/uni-directional update between the 2 folders',
+        default: 'update-both',
+    }).argv;
 
 const [src, dest] = argv._ as [string, string];
+
+let includescMatcher: Ignore;
+if (argv.files) {
+    includescMatcher = ignore();
+    argv.files.forEach((pattern: string) => {
+        includescMatcher.add(pattern);
+    });
+}
+
+let ignoresMatcher: Ignore;
+const ignoreFile = join(src, '.msfignore');
+if (existsSync(ignoreFile)) {
+    ignoresMatcher = ignore().add(readFileSync(ignoreFile).toString());
+}
 
 const getSubscriptionHandler = (source: string, target: string, logger: Logger): Handler => {
     return (info) => {
         if (info.type !== 'f') {
             return;
         }
+        // try includes
+        if (includescMatcher && !includescMatcher.ignores(info.name)) {
+            return;
+        }
+        // try ignores
+        if (ignoresMatcher && ignoresMatcher.ignores(info.name)) {
+            return;
+        }
+
         if (info.exists) {
             syncTo(
                 {
@@ -51,8 +88,16 @@ const getSubscriptionHandler = (source: string, target: string, logger: Logger):
     };
 };
 
+// remove the target dir files first - TODO: should compare and do necessary removal for better performance
+if (argv.mode === 'mirror') {
+    removeSync(dest);
+    ensureDirSync(dest);
+}
+
 const srcLogger = getLogger((text) => green(`=> ${text}`));
 startWatch(src, getSubscriptionHandler(src, dest, srcLogger), srcLogger);
 
-const destLogger = getLogger((text) => cyan(`<= ${text}`));
-startWatch(dest, getSubscriptionHandler(dest, src, destLogger), destLogger);
+if (argv.mode === 'update-both') {
+    const destLogger = getLogger((text) => cyan(`<= ${text}`));
+    startWatch(dest, getSubscriptionHandler(dest, src, destLogger), destLogger);
+}
